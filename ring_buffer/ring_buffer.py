3cb95d0035c84ab4e0703800313575483840916d
kingmoc <joe.c.a.garcia@gmail.com>
2019-11-15 11:46:34 -0600

    Finished ring_buffer.py - Test Passed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,31 +5,37 @@ class RingBuffer:
 		self.storage = [None]*capacity
 
 	def append(self, item):
-		# if 
 		for i in range(0, len(self.storage)):
 			if self.storage[i] is None:
+				print('in if')
 				self.storage[i] = item
 				return
-			# else:
-			# 	print('in else')
-				# self.storage[0] = item
+
+		if not None in self.storage:
+			self.storage[self.current] = item
+		
+		self.current += 1
+
+		if self.current is self.capacity:
+			self.current = 0
 
 	def get(self):
 		new_list = [item for item in self.storage if item is not None]
 		
-		print(new_list, 'st')
+		return new_list
 
 
-r = RingBuffer(5)
+# r = RingBuffer(5)
 
-r.append('a')
-r.append('b')
-r.append('c')
-r.append('d')
-r.append('e')
-# Past Capacity - should remove oldest (A)
-r.append('f') # get should return ['f', 'b', 'c', 'd', 'e']
+# r.append('a')
+# r.append('b')
+# r.append('c')
+# r.append('d')
+# r.append('e')
+# # Past Capacity - should remove oldest (A)
+# r.append('f') # get should return ['f', 'b', 'c', 'd', 'e']
 # r.append('g') # get should return ['f', 'g', 'c', 'd', 'e']
+# r.append('h') # get should return ['f', 'g', 'h', 'd', 'e']
 
 # print(r.storage)
-r.get() 
+# print(r.get())
